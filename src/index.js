c1b1b79b2a3821ba3b7e273b5538ed331575b079
Olli Eskola <olli.eskola@siili.com>
2018-10-24 09:25:56 +0300

    tehtävät 2.1 - 2.5

--- src/index.js
+++ src/index.js
@@ -1,68 +1,62 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Kurssi from './kurssi.js'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      selected: 0,
-      votes: {0:0, 1:0, 2:0, 3:0, 4:0, 5:0}
-    }
-  }
-
-  setRandom = () => {
-    this.setState({
-      selected: Math.floor(Math.random() * Math.floor(anecdotes.length-1))
-    })
-  }
-
-  setVote = (index) => () => {
-    let property = this.state.votes
-    property[index] = property[index] + 1
-    this.setState({
-      votes: property 
-    })
-  }
-
-  render() {
-    const anecdote = anecdotes[this.state.selected]
-
-    let max = 0
-    let indexOfMax = null
-    for(let a in this.state.votes) {
-      if(this.state.votes[a] > max) {
-        max = this.state.votes[a]
-        indexOfMax = a
-      }
-    }
-
-    return (
-      <div>
-        <p>{anecdote}</p>
-        <p> has {this.state.votes[this.state.selected]} votes</p>
-        <div>
-          <button onClick={this.setVote(this.state.selected)}>vote</button>
-          <button onClick={this.setRandom}>Next anecdote</button>
-        </div>
-        <div>
-          <h2> anecdote with most votes: </h2>
-          <p> { anecdotes[indexOfMax] }</p>
-          <p> has {this.state.votes[indexOfMax]} votes</p>
-        </div>
-      </div>
-    )
-  }
+const Kurssilista = (props) => {
+  return (
+    <div>
+      {props.kurssit.map(a => <Kurssi key={a.id} kurssi={a}/>)}
+    </div>
+  )
 }
 
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-]
+const App = () => {
+  const kurssit = [
+    {
+      nimi: 'Half Stack -sovelluskehitys',
+      id: 1,
+      osat: [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10,
+          id: 1
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7,
+          id: 2
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      nimi: 'Node.js',
+      id: 2,
+      osat: [
+        {
+          nimi: 'Routing',
+          tehtavia: 3,
+          id: 1
+        },
+        {
+          nimi: 'Middlewaret',
+          tehtavia: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
+  return (
+    <div>
+      <Kurssilista kurssit={kurssit} />
+    </div>
+  )
+}
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
