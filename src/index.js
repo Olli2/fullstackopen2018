b15dd4f8a30a8aefc8655ac0eada44b18ac38b88
Olli Eskola <olli.eskola@siili.com>
2018-10-24 12:20:20 +0300

    tehtävät 2.6 - 2.10

--- src/index.js
+++ src/index.js
@@ -1,62 +1,116 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom'
-import Kurssi from './kurssi.js'
 
-const Kurssilista = (props) => {
+const Numero = (props) => {
+  return (
+     <tr>
+      <td>{props.info.name}</td>
+      <td>{props.info.number}</td>
+     </tr>
+  )
+}
+
+const Filtteri = (props) => {
   return (
     <div>
-      {props.kurssit.map(a => <Kurssi key={a.id} kurssi={a}/>)}
+    Rajaa näytettäviä: <input value={props.filter} onChange={props.handleFilterChange}/>
     </div>
   )
 }
 
-const App = () => {
-  const kurssit = [
-    {
-      nimi: 'Half Stack -sovelluskehitys',
-      id: 1,
-      osat: [
-        {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10,
-          id: 1
-        },
-        {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7,
-          id: 2
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14,
-          id: 3
-        }
-      ]
-    },
-    {
-      nimi: 'Node.js',
-      id: 2,
-      osat: [
-        {
-          nimi: 'Routing',
-          tehtavia: 3,
-          id: 1
-        },
-        {
-          nimi: 'Middlewaret',
-          tehtavia: 7,
-          id: 2
-        }
-      ]
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas', number: 34 }
+      ],
+      filter: '',
+      newName: '',
+      newNumber: '',
     }
-  ]
 
-  return (
-    <div>
-      <Kurssilista kurssit={kurssit} />
-    </div>
-  )
+
+  }
+
+  handleFilterChange = (event) => {
+    this.setState({
+      filter: event.target.value
+    })
+  }
+
+  handleNewNumber = (event) => {
+    this.setState({
+      newNumber: event.target.value
+    })
+  }
+
+  handleNewName = (event) => {
+    this.setState({
+      newName: event.target.value
+    })
+  }
+
+  handleNameSubmit = (event) => {
+    event.preventDefault()
+    if(!this.state.persons.find(a => a.name === this.state.newName)) {
+      const persons = this.state.persons.concat({name: this.state.newName, number: this.state.newNumber})
+      this.setState({
+        persons,
+        newName: '',
+        newNumber: ''
+      })
+    }else {
+      alert(`Nimi '${this.state.newName}' on jo listalla`)
+      this.setState({
+        newName: ''
+      })
+    }
+    
+  }
+
+  render() {
+    const personsToShow = 
+      this.state.filter === '' ?
+            this.state.persons :
+            this.state.persons.filter(a => a.name.toLowerCase().includes(this.state.filter.toLowerCase()))
+
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <Filtteri filter={this.state.filter} handleFilterChange={this.handleFilterChange}/>
+
+        <form onSubmit={this.handleNameSubmit}>
+          <div>
+            nimi: <input value={this.state.newName} onChange={this.handleNewName} />
+          </div>
+          <div>
+            numero: <input type="number" value={this.state.newNumber} onChange={this.handleNewNumber} />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        <div>
+          <table>
+            <thead>
+            <tr>
+              <th> Nimi </th>
+              <th> Numero </th>
+            </tr>
+            </thead>
+            <tbody>
+              {personsToShow.map(e => <Numero key={e.name} info={e}/>)}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
 }
+
+// export default App
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
